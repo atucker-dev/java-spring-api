1e734819b8dcee1fb2a928bb4b8d397941fc60e6
atucker-dev <actkant@gmail.com>
2026-04-08 12:54:49 -0500

    feat: add initial shell springboot code

---
+++ src/test/java/mai/javaspring/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mai.javaspring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
